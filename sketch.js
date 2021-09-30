a8dd289310f6fb288689e76c1d73d57c7aa5cf5d
RubyGitHubber09 <90994231+RubyGitHubber09@users.noreply.github.com>
2021-09-29 19:36:13 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,40 +1,18 @@
-
+var weight = [35,38,42,45,43,34,36,41,48,32];
+var sum = 0 
 function setup() {
   createCanvas(400,400);
-  background(51);
-  box = createSprite(200,200,30,30);
+for (var i=0; i<weight.length; i++) {
+  sum = sum + weight [i]
+} 
+console.log (sum)
 
 }
 
 function draw() 
 {
-
-  // write the code to change background color 
-  // to red when RIGHT_ARROW is pressed
-  if (keyIsDown (RIGHT_ARROW))
-  {
-    background ("red")
-  }
-
-  if (keyIsDown(LEFT_ARROW)) 
-  {
-    background("blue");
-    
-  }
- 
-    if (keyIsDown(UP_ARROW)) 
-  {
-    background("yellow");
-   
-  }
-
-  if (keyIsDown(DOWN_ARROW)) 
-  {
-    background("green");
-  }
-
-
-  
-  drawSprites();
+  background(30);
 }
+
+ 
 
